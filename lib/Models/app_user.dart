36ba47a8477cac23d6a532d0ef8690b23aea96a1
Hasan Berke Tekin <hbt19031@gmail.com>
2021-03-22 01:43:45 +0300

    Create AppUser model

---
+++ lib/Models/app_user.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final _firestore = FirebaseFirestore.instance;
+
+class AppUser with ChangeNotifier {
+  static AppUser instance = AppUser._();
+  String uid, name, email, photo, type;
+
+  bool get isMentor => type == "Mentor";
+  bool get isMentee => type == "Mentee";
+
+  DocumentReference get docRef => _firestore.collection("users").doc(uid);
+
+  AppUser._();
+
+  setUid(String _uid) {
+    uid = _uid;
+  }
+
+  setData(Map<String, dynamic> data) {
+    name = data["name"] ?? "";
+    email = data["email"] ?? "";
+    photo = data["photo"] ?? "";
+    type = data["type"] ?? "";
+  }
+}
