d5a4218612f00e3e7d2f42c9ccb3270d5eff493a
Hasan Berke Tekin <hbt19031@gmail.com>
2021-03-23 03:31:14 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,11 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mentspire/Controllers/auth_controller.dart';
 import 'package:mentspire/Screens/App/home_screen.dart';
-import 'package:mentspire/Screens/Auth/login_screen.dart';
+import 'package:mentspire/Screens/Auth/country_select_screen.dart';
 import 'package:get/get.dart';
 import 'package:mentspire/Screens/Auth/register_screen.dart';
 import 'package:mentspire/Screens/Auth/reset_password_screen.dart';
+import 'package:mentspire/Screens/Auth/skills_screen.dart';
 import 'package:mentspire/Screens/splash_screen.dart';
 import 'package:mentspire/Themes/colors.dart';
 import 'package:mentspire/Themes/input_decoration_theme.dart';
@@ -43,70 +44,23 @@ class MyApp extends StatelessWidget {
       getPages: [
         GetPage(name: '/', page: () => SplashScreen()),
         GetPage(name: '/home', page: () => HomeScreen()),
-        GetPage(name: '/login', page: () => LoginScreen()),
+        GetPage(name: '/login', page: () => SkillsScreen()),
         GetPage(name: "/register", page: () => RegisterScreen()),
         GetPage(name: "/reset_password", page: () => ResetPasswordScreen()),
+        GetPage(name: "/country_select", page: () => CountrySelectScreen()),
       ],
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+/*uploadUniversities() async {
+  String jsonText = await rootBundle.loadString('assets/universities.json');
+  List<dynamic> data = json.decode(jsonText);
+  print("${data.length} universities read");
+  CollectionReference _ref =
+      FirebaseFirestore.instance.collection("universities");
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          color: Colors.black12,
-          child: Center(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(
-                  'You have pushed the button this many times:',
-                ),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.headline4,
-                ),
-                Container(
-                  padding: EdgeInsets.all(16),
-                  color: Colors.amber,
-                  child: Column(
-                    children: [Text("deneme"), Text("deneme"), Text("deneme")],
-                  ),
-                ),
-                InkWell(
-                  onTap: () {
-                    print("image tapped");
-                  },
-                  child: Image.network(
-                    "https://i1.wp.com/www.muratoner.net/wp-content/uploads/2019/01/flutterlogo.png",
-                    width: 50,
-                    height: 50,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
+  data.forEach((element) {
+    _ref.add((element as Map<String, dynamic>));
+  });
+}*/
